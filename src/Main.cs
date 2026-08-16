8caa02960535d962cf671d146ea79c5ba8df8b69
wasdeilonn <sjamsuddin.dilon@gmail.com>
2026-08-16 17:16:41 +0200

    no save lmao

--- src/Main.cs
+++ src/Main.cs
@@ -6,7 +6,6 @@ using Polytopia.Data;
 using PolytopiaBackendBase.Common;
 using Polibrary.PolyScript;
 using Il2Gen = Il2CppSystem.Collections.Generic;
-using DG.Tweening;
 
 namespace IllusionJamMod;
 
@@ -22,38 +21,39 @@ public static class Main
     static UnitData.Type RealUnit;
     static UnitData.Type FakeUnit;
     static UnitAbility.Type IllusionAbil;
-    public static Dictionary<UnitState, UnitState> Pairs = new();
+    public static Dictionary<uint, uint> Pairs = new();
 
     public static void SetPair(this UnitState inst, UnitState pair)
     {
-        Pairs[inst] = pair;
-        Pairs[pair] = inst;
+        Pairs[inst.id] = pair.id;
+        Pairs[pair.id] = inst.id;
     }
     
-    public static void RemovePair(this UnitState inst)
+    public static void RemovePair(this UnitState inst, GameState state)
     {
-        Pairs.Remove(inst.GetPair());
-        Pairs.Remove(inst);
+        Pairs.Remove(inst.GetPair(state).id);
+        Pairs.Remove(inst.id);
     }
 
-    public static UnitState GetPair(this UnitState inst)
+    public static UnitState GetPair(this UnitState inst, GameState state)
     {
-        if (Pairs.TryGetValue(inst, out var pair))
+        if (Pairs.TryGetValue(inst.id, out var pair))
         {
-            return pair;
+            if (state.TryGetUnit(pair, out var pairUnit))
+            return pairUnit;
         }
         return null;
     }
-    public static bool TryGetPair(this UnitState inst, out UnitState pair)
+    public static bool TryGetPair(this UnitState inst, out UnitState pair, GameState state)
     {
-        if (inst.GetPair() == null)
+        if (inst.GetPair(state) == null)
         {
             pair = null;
             return false;
         }
         else
         {
-            pair = inst.GetPair();
+            pair = inst.GetPair(state);
             return true;
         }
     }
@@ -70,16 +70,9 @@ public static class Main
         {
             modLogger.LogError("couldnt find enum stuff");
         }
-    }
 
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(UnitDataExtensions), nameof(UnitDataExtensions.GetMovementOptions))]
-    private static void DisableMoveForDummy(UnitState unit, GameState gameState, int range, ref Il2Gen.List<WorldCoordinates> __result)
-    {
-        if (unit.type == FakeUnit)
-        {
-            __result = new Il2Gen.List<WorldCoordinates>();
-        }
+        PolibActionManager.RegisterAction<ApplyRadiantAction>("applyradiantaction");
+        PolibReactionManager.AssignReaction<ApplyRadiantReaction>("applyradiantaction");
     }
 
     [HarmonyPostfix]
@@ -94,14 +87,13 @@ public static class Main
             !state.GameLogicData.TryGetData(FakeUnit, out var fakeUnitData)
             )
         {
-            modLogger.LogError("idiot");
             return;
         }
         if (unit.type == RealUnit)
         {
-            if (unit.TryGetPair(out var dummy))
+            if (unit.TryGetPair(out var dummy, state))
             {
-                unit.RemovePair();
+                unit.RemovePair(state);
                 state.Map.GetTile(dummy.coordinates).SetUnit(null);
             }
 
@@ -110,6 +102,21 @@ public static class Main
             UnitState newDummy = ActionUtils.TrainUnit(state, player, state.Map.GetTile(originCoords), fakeUnitData);
             newDummy.MakeExhauseted(state);
             unit.SetPair(newDummy);
+        }
+        if (unit.type == FakeUnit)
+        {
+            if (!unit.TryGetPair(out var real, state))
+            {
+                modLogger.LogError("nice one shitsmear");
+                return;
+            }
+
+            WorldCoordinates originCoords = __instance.Path[__instance.Path.Count - 1];
+
+            state.Map.GetTile(real.coordinates).SetUnit(null);
+            state.Map.GetTile(originCoords).SetUnit(real);
+            real.coordinates = originCoords;
+            real.MakeExhauseted(state);
         }
     }
 
@@ -124,16 +131,228 @@ public static class Main
             modLogger.LogError("idiot");
             return;
         }
-        if (unit.type == RealUnit)
+
+        if (unit.type == RealUnit || unit.type == FakeUnit)
         {
             WorldCoordinates originCoords = __instance.action.Path[__instance.action.Path.Count - 1];
+            Tile fromTile = MapRenderer.Current.GetTileInstance(originCoords);
+            fromTile.Render();
 
-            foreach (TileData tileData in GameManager.GameState.Map.GetArea(originCoords, 3, true, true))
+            foreach (TileData tileData in GameManager.GameState.Map.GetArea(originCoords, 2, true, true))
             {
                 Tile tile = MapRenderer.Current.GetTileInstance(tileData.coordinates);
                 if (tile == null || tile.IsHidden) continue;
                 tile.Render();
             }
+        }
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(KillUnitAction), nameof(KillUnitAction.Execute))]
+    private static void KillUnitPatch(GameState gameState, KillUnitAction __instance)
+    {
+        UnitState unit = gameState.Map.GetTile(__instance.Coordinates).unit;
+        if (unit == null) return;
+
+        if (unit.type == RealUnit)
+        {
+            if (unit.TryGetPair(out var dummy, gameState))
+            {
+                __instance.AddSubAction(new KillUnitAction(__instance.PlayerId, dummy.coordinates));
+            }
+        }
+        if (unit.type == FakeUnit)
+        {
+            if (unit.TryGetPair(out var real, gameState))
+            {
+                ApplyRadiantAction action = PolibActionManager.MakeIl2CppAction<ApplyRadiantAction>();
+                action.PlayerId = __instance.PlayerId;
+                action.Coordinates = real.coordinates;
+                __instance.AddSubAction(action);
+            }
+        }
+        __instance.CommitSubActionsToStack(gameState.ActionStack);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(InfiltrateAction), nameof(InfiltrateAction.Execute))]
+    private static void InfiltratePatch(GameState gameState, InfiltrateAction __instance)
+    {
+        UnitState unit = gameState.Map.GetTile(__instance.Origin).unit;
+        if (unit == null) return;
+
+        if (unit.type == RealUnit)
+        {
+            if (unit.TryGetPair(out var dummy, gameState))
+            {
+                __instance.AddSubAction(new KillUnitAction(__instance.PlayerId, dummy.coordinates));
+            }
+        }
+        __instance.CommitSubActionsToStack(gameState.ActionStack);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(StartTurnAction), nameof(StartTurnAction.Execute))]
+    private static void HideRadiantUnitsPatch(GameState state, StartTurnAction __instance)
+    {
+        foreach (TileData tile in state.Map.tiles)
+        {
+            if (tile.unit == null) continue;
+            if (tile.unit.owner != __instance.PlayerId) continue;
+            if (tile.unit.HasEffect(EnumCache<UnitEffect>.GetType("radiant")))
+            {
+                __instance.AddSubAction(new HideAction(__instance.PlayerId, tile.coordinates));
+            }
+        }
+        __instance.CommitSubActionsToStack(state.ActionStack);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(HideAction), nameof(HideAction.Execute))]
+    private static void RemoveRadiancePatch(GameState state, HideAction __instance)
+    {
+        UnitState unit = state.Map.GetTile(__instance.Coordinates).unit;
+        if (unit == null) return;
+
+        unit.RemoveEffect(EnumCache<UnitEffect>.GetType("radiant"));
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(UnitDataExtensions), nameof(UnitDataExtensions.GetAttackOptions))]
+    private static void DummyCantAttack(this UnitState unit, GameState state, ref int range, bool ignoreDiplomacyRelation = false)
+    {
+        if (unit.type == FakeUnit)
+        {
+            range = 0;
+        }
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Unit), nameof(Unit.OnSelected))]
+    private static void SwayPairs(Unit __instance)
+    {
+        if (__instance.unitState.TryGetPair(out var pair, GameManager.GameState))
+        {
+            Tile inst = MapRenderer.Current.GetTileInstance(pair.coordinates);
+            inst.Render();
+            GameManager.DelayCall(0.07f, (Il2CppSystem.Action) delegate {inst.Unit.Sway();});
+        }
+    }
+
+    /*[HarmonyPostfix]
+    [HarmonyPatch(typeof(UnitState), nameof(UnitState.SerializeDefault))]
+    private static void UnitSerialize(Il2CppSystem.IO.BinaryWriter writer, int version, UnitState __instance)
+    {
+        writer.Write(Pairs.GetValueOrDefault(__instance.id, uint.MaxValue));
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(UnitState), nameof(UnitState.DeserializeDefault))]
+    private static void UnitDeserialize(Il2CppSystem.IO.BinaryReader reader, int version, UnitState __instance)
+    {
+        uint pairId = reader.ReadUInt32();
+
+        if (pairId != uint.MaxValue)
+        {
+            Pairs[__instance.id] = pairId;
+        }
+    }*/
+}
+
+public class ApplyRadiantAction : PolibActionBase
+{
+    public WorldCoordinates Coordinates;
+    public ApplyRadiantAction(IntPtr ptr) : base(ptr) {}
+    public ApplyRadiantAction() {}
+    
+    public override bool IsValid(GameState state)
+    {
+        return true;
+    }
+
+    public override ActionType GetActionType()
+    {
+        return EnumCache<ActionType>.GetType("applyradiantaction");
+    }
+    
+    public override void Execute(GameState state)
+    {
+        TileData tile = state.Map.GetTile(Coordinates);
+        if (tile.unit != null)
+        {
+            tile.unit.AddEffect(EnumCache<UnitEffect>.GetType("radiant"));
+        }
+    }
+
+    public override void Serialize(Il2CppSystem.IO.BinaryWriter writer, int version)
+    {
+        writer.Write(PlayerId); //this line is important btw
+        Coordinates.Serialize(writer, version);
+    }
+
+    public override void Deserialize(Il2CppSystem.IO.BinaryReader reader, int version)
+    {
+        PlayerId = reader.ReadByte(); //leave this line in
+        Coordinates.Deserialize(reader, version);
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0} (PlayerId: {1}, Coordinates: {2})", new object[]
+        {
+            base.GetType(),
+            base.PlayerId,
+            this.Coordinates
+        });
+    }
+}
+
+public class ApplyRadiantReaction : PolibReactionBase
+{
+    protected ApplyRadiantAction action;
+    public override ActionBase actionProperty 
+    { 
+        get => this.action; 
+        set
+        {
+            ApplyRadiantAction polibActionBase = value.TryCast<ApplyRadiantAction>();
+            if (polibActionBase != null)
+            this.action = polibActionBase;
+            else
+            Main.modLogger.LogInfo("shits fucked");
+        } 
+    }
+    public ApplyRadiantReaction(IntPtr ptr) : base(ptr) {}
+    public ApplyRadiantReaction(ApplyRadiantAction action)
+    {
+        this.action = action;
+    }
+
+    public override bool ShouldFocusCamera()
+    {
+        return IsRecapOrOpponentAction(action);
+    }
+
+    public override WorldCoordinates GetCameraFocusCoordinates()
+    {
+        return action.Coordinates;
+    }
+
+    public override void Execute(Il2CppSystem.Action onComplete)
+    {
+        TileData tile = GameManager.GameState.Map.GetTile(action.Coordinates);
+        Tile instance = tile.GetInstance();
+        if (instance != null && !instance.IsHidden)
+        {
+            instance.Render();
+            instance.SpawnDarkPuff();
+            instance.SpawnEmbers();
+            AudioManager.PlaySFXAtTile(SFXTypes.Burn, tile.coordinates);
+            GameManager.DelayCall(0.2f, onComplete);
+        }
+        else
+        {
+            onComplete.Invoke();
         }
     }
 }
